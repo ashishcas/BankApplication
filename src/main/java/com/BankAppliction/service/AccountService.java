25efc1cc3dfadac8b626bfeceb79b117eab599b0
ashishcas <chilukaashish@gmail.com>
2021-08-15 00:07:40 +0530

    feat:: adding kafka

--- src/main/java/com/BankAppliction/service/AccountService.java
+++ src/main/java/com/BankAppliction/service/AccountService.java
@@ -5,7 +5,7 @@ import com.BankAppliction.common.CreateBankAccountMapper;
 import com.BankAppliction.model.BankAccount;
 
 public interface AccountService {
-    BankAccount createAccount(CreateBankAccountMapper account, String token);
+    BankAccount createAccount(CreateBankAccountMapper account, String token) throws Exception;
 
     BankAccount updateAccount(BankAccount account);
 
